f4121b6e1e4617ece25618ccf19b2b8045a6d021
alpeters <alpeters@users.noreply.github.com>
2022-06-10 20:59:45 -0700

    indexing with CC

--- src/AIS_Data_Index.py
+++ src/AIS_Data_Index.py
@@ -1,5 +1,4 @@
 # Set index by mmsi so future operations can be partition-wise
-
 import dask.dataframe as dd
 import pandas as pd
 import numpy as np
@@ -8,7 +7,7 @@ import os, time
 # from dask.distributed import Client, LocalCluster
 # cluster = LocalCluster(n_workers=1)
 
-filepath = 'src/data/AIS'
+filepath = '/scratch/petersal/ShippingEmissions/src/data/AIS'
 # ais = pd.read_parquet(os.path.join(filepath, 'aisparquet/part.0.parquet'))
 ais_bulkers = dd.read_parquet(os.path.join(filepath, 'ais_bulkers'))
 # dd.compute(ais_bulkers.shape)
@@ -17,16 +16,20 @@ unique_mmsi.sort_values(inplace = True, ignore_index = True)
 breakpoints = list(np.linspace(unique_mmsi[0], unique_mmsi.iat[-1], 40).astype('int32'))
 # Need to set divisions because automatic algorithm seems to give floats
 breaks_to_keep = [0, 2, 4, 8, 9, 11, 12, 15, 18, 19, 24, 30, 39]
-breakpoints = [ even_breakpoints[i] for i in breaks_to_keep]
+breakpoints = [breakpoints[i] for i in breaks_to_keep]
 
 
 # Set index so data is sorted by mmsi
 print("Setting index to mmsi")
 start = time.time()
 print("Starting at: ", start)
+outpath = os.path.join(filepath, 'ais_bulkers_indexed')
+if not os.path.exists(outpath):
+	os.mkdir(outpath)
+#ais_bulkers.head().to_csv(os.path.join(outpath, 'test.csv'))
 (
-ais_bulkers.set_index('mmsi', shuffle = 'disk', divisions = breakpoints)
-.to_parquet(os.path.join(filepath, 'ais_bulkers_mmsi'),
+	ais_bulkers.set_index('mmsi', shuffle = 'disk', divisions = breakpoints)
+	.to_parquet(os.path.join(filepath, 'ais_bulkers_indexed'),
             append = False,
             overwrite = True)
 )
