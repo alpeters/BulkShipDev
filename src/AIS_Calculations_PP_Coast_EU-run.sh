c2ba511faf8ea42ecac568c975a8177acec91aed
alpeters <alpeters@users.noreply.github.com>
2025-07-10 14:42:30 -0400

    correct bash run file

--- src/AIS_Calculations_PP_Coast_EU-run.sh
+++ src/AIS_Calculations_PP_Coast_EU-run.sh
@@ -6,7 +6,16 @@
 #SBATCH --time=1:00:0    
 
 module load StdEnv/2023 arrow/19.0.1 python/3.12
+
+srun --ntasks $SLURM_NNODES --tasks-per-node=1 bash << EOF
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
 pip install --no-index -r python_requirements_ShipDist_CC.txt
-python AIS_Calculations_PP_Coast_EU.py
+EOF
+
+# activate only on the main node                                                               
+source $SLURM_TMPDIR/env/bin/activate;
+# srun exports the current env, which contains $VIRTUAL_ENV and $PATH variables
+srun python AIS_Calculations_PP_Coast_EU.py
